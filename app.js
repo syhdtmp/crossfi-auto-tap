1e0cccd5c62857c04af10a70adf8bb45a09f7f15
syhdtmp <syhdtmp@gmail.com>
2024-05-25 10:24:16 +0700

    feat: update socket behaviour

--- app.js
+++ app.js
@@ -18,122 +18,134 @@ const WITHDRAWAL_URL = 'https://test-bot.crossfi.org/api/v1/user/wallet/withdraw
 
 const HEARTBEAT_INTERVAL = 25000;
 const SIMULATION_INTERVAL = 30000;
+const RETRY_INTERVAL = 5000;
 
 JWT_TOKENS.forEach(({ token, workerId }) => {
-  const socket = new WebSocket(SOCKET_URL, { headers: SOCKET_HEADERS });
+  let socket;
 
-  socket.onopen = () => {
-    prettyLog(`[${workerId}] Connection established`);
-    socket.send("40" + JSON.stringify({ "token": token }));
-    prettyLog(`[${workerId}] Sent token message`);
+  function connectWebSocket() {
+    socket = new WebSocket(SOCKET_URL, { headers: SOCKET_HEADERS });
 
-    setInterval(() => {
-      socket.send("3");
-      prettyLog(`[${workerId}] Sent message "3" to the server`);
-    }, HEARTBEAT_INTERVAL);
+    socket.onopen = () => {
+      prettyLog(`[${workerId}] Connection established`);
+      socket.send("40" + JSON.stringify({ "token": token }));
+      prettyLog(`[${workerId}] Sent token message`);
 
-    let currentBaseDate = 12096.5;
-    let energyBalance = 0;
+      setInterval(() => {
+        socket.send("3");
+        prettyLog(`[${workerId}] Sent message "3" to the server`);
+      }, HEARTBEAT_INTERVAL);
+    };
 
-    async function fetchEnergyBalance() {
-      prettyLog(`[${workerId}] Attempting to fetch energy balance...`);
-      try {
-        const response = await fetch(BALANCE_URL, {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'x-authorization': token
-          }
-        });
-        const data = await response.json();
-        energyBalance = parseInt(data.energy);
-        prettyLog(`[${workerId}] Energy balance updated to ${energyBalance}.`);
-      } catch (error) {
-        prettyLog(`[${workerId}] Failed to fetch energy balance.`, 'error');
-      }
-    }
-    function generateSimulationData() {
-      prettyLog(`[${workerId}] Generating simulation data...`);
-      const interval = 1000;
-      let simulationData = { taps: [] };
-      let clicksPossible = Math.min(SIMULATION_INTERVAL / interval, energyBalance);
+    socket.onerror = (error) => {
+      prettyLog(`[${workerId}] WebSocket Error: ${error}`, 'error');
+      setTimeout(connectWebSocket, RETRY_INTERVAL);
+      prettyLog(`[${workerId}] Attempting to reconnect...`);
+    };
 
-      if (clicksPossible >= 10) {
-        for (let i = 0; i < clicksPossible; i++) {
-          simulationData.taps.push({
-            actions: "CLICK",
-            date: currentBaseDate + i * interval
-          });
-        }
-        currentBaseDate += clicksPossible * interval;
-        energyBalance -= clicksPossible;
-        prettyLog(`[${workerId}] Generated ${clicksPossible} clicks for simulation.`);
+    socket.onmessage = (event) => {
+      prettyLog(`[${workerId}] Message from server: ${event.data}`, 'success');
+    };
+
+    socket.onclose = (event) => {
+      if (event.wasClean) {
+        prettyLog(`[${workerId}] WebSocket closed cleanly, code=${event.code}, reason=${event.reason}`);
       } else {
-        prettyLog(`[${workerId}] Less than 10 clicks possible, no simulation data generated.`);
+        prettyLog(`[${workerId}] WebSocket closed unexpectedly.`, 'error');
+        setTimeout(connectWebSocket, RETRY_INTERVAL);
+        prettyLog(`[${workerId}] Attempting to reconnect...`);
       }
-      return simulationData;
-    }
+    };
+  }
 
-    async function sendSimulationData(simulationData) {
-      if (simulationData.taps.length > 0) {
-        prettyLog(`[${workerId}] Sending simulation data...`);
-        const message = JSON.stringify(["TAP", simulationData]);
-        socket.send("42" + message);
-        prettyLog(`[${workerId}] Simulation data sent with ${simulationData.taps.length} taps.`);
-      } else {
-        prettyLog(`[${workerId}] No simulation data sent as there are no taps.`);
-      }
+  connectWebSocket();
 
-      let amount = simulationData.taps.length * 0.5;
-      let data = JSON.stringify({ "amount": amount });
-      let config = {
-        method: 'POST',
+  let currentBaseDate = 1290;
+  let energyBalance = 0;
+
+  async function fetchEnergyBalance() {
+    prettyLog(`[${workerId}] Attempting to fetch energy balance...`);
+    try {
+      const response = await fetch(BALANCE_URL, {
+        method: 'GET',
         headers: {
-          'Content-Type': 'application/json',
+          'Accept': 'application/json',
           'x-authorization': token
-        },
-        body: data
-      };
-
-      if (amount > 0) {
-        prettyLog(`[${workerId}] Sending withdrawal request...`);
-        try {
-          const response = await fetch(WITHDRAWAL_URL, config);
-          const responseData = await response.json();
-          if (responseData.type === "invalid_request_error" && responseData.code === "low_balance" && responseData.message === "Insufficient funds.") {
-            prettyLog(`[${workerId}] Withdrawal request failed: Insufficient funds.`, 'error');
-          } else {
-            prettyLog(`[${workerId}] Withdrawal request processed for amount $${amount}.`);
-          }
-        } catch (error) {
-          prettyLog(`[${workerId}] Failed to process withdrawal request.`, 'error');
         }
-      } else {
-        prettyLog(`[${workerId}] No withdrawal made due to zero amount.`);
+      });
+      const data = await response.json();
+      energyBalance = parseInt(data.energy);
+      prettyLog(`[${workerId}] Energy balance updated to ${energyBalance}.`);
+    } catch (error) {
+      prettyLog(`[${workerId}] Failed to fetch energy balance.`, 'error');
+    }
+  }
+
+  function generateSimulationData() {
+    prettyLog(`[${workerId}] Generating simulation data...`);
+    const interval = Math.floor(Math.random() * (1000 - 500 + 1) + 500);
+    let simulationData = { taps: [] };
+    let clicksPossible = Math.min(Math.floor(Math.random() * (50 - 30 + 1) + 30), energyBalance);
+
+    if (clicksPossible >= 10) {
+      for (let i = 0; i < clicksPossible; i++) {
+        simulationData.taps.push({
+          actions: "CLICK",
+          date: currentBaseDate + i * interval
+        });
       }
-    }
-
-    setInterval(async () => {
-      prettyLog(`[${workerId}] Interval triggered: Processing tasks...`);
-      await fetchEnergyBalance();
-      const newSimulationData = generateSimulationData();
-      await sendSimulationData(newSimulationData);
-    }, SIMULATION_INTERVAL);
-  };
-
-  socket.onerror = (error) => {
-    prettyLog(`[${workerId}] WebSocket Error: ${error}`, 'error');
-  };
-
-  socket.onmessage = (event) => {
-    prettyLog(`[${workerId}] Message from server: ${event.data}`, 'success');
-  };
-
-  socket.onclose = (event) => {
-    if (event.wasClean) {
-      prettyLog(`[${workerId}] WebSocket closed cleanly, code=${event.code}, reason=${event.reason}`);
+      currentBaseDate += clicksPossible * interval;
+      energyBalance -= clicksPossible;
+      prettyLog(`[${workerId}] Generated ${clicksPossible} clicks for simulation.`);
     } else {
-      prettyLog(`[${workerId}] WebSocket closed unexpectedly.`, 'error');
+      prettyLog(`[${workerId}] Less than 10 clicks possible, no simulation data generated.`);
     }
-  };
+    return simulationData;
+  }
+
+  async function sendSimulationData(simulationData) {
+    if (simulationData.taps.length > 0) {
+      prettyLog(`[${workerId}] Sending simulation data...`);
+      const message = JSON.stringify(["TAP", simulationData]);
+      socket.send("42" + message);
+      prettyLog(`[${workerId}] Simulation data sent with ${simulationData.taps.length} taps.`);
+    } else {
+      prettyLog(`[${workerId}] No simulation data sent as there are no taps.`);
+    }
+
+    let amount = simulationData.taps.length * 0.5;
+    let data = JSON.stringify({ "amount": amount });
+    let config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-authorization': token
+      },
+      body: data
+    };
+
+    if (amount > 0) {
+      prettyLog(`[${workerId}] Sending withdrawal request...`);
+      try {
+        const response = await fetch(WITHDRAWAL_URL, config);
+        const responseData = await response.json();
+        if (responseData.type === "invalid_request_error" && responseData.code === "low_balance" && responseData.message === "Insufficient funds.") {
+          prettyLog(`[${workerId}] Withdrawal request failed: Insufficient funds.`, 'error');
+        } else {
+          prettyLog(`[${workerId}] Withdrawal request processed for amount $${amount}.`);
+        }
+      } catch (error) {
+        prettyLog(`[${workerId}] Failed to process withdrawal request.`, 'error');
+      }
+    } else {
+      prettyLog(`[${workerId}] No withdrawal made due to zero amount.`);
+    }
+  }
+
+  setInterval(async () => {
+    prettyLog(`[${workerId}] Interval triggered: Processing tasks...`);
+    await fetchEnergyBalance();
+    const newSimulationData = generateSimulationData();
+    await sendSimulationData(newSimulationData);
+  }, SIMULATION_INTERVAL);
 });
